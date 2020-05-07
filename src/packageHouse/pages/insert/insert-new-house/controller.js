59ea1473b5e3120f9a429cc868ba538355df1e18
penglunjian <875831103@qq.com>
2020-05-08 00:26:28 +0800

    1、项目优化

--- src/packageHouse/pages/insert/insert-new-house/controller.js
+++ src/packageHouse/pages/insert/insert-new-house/controller.js
@@ -4,7 +4,7 @@ export const STATES = {
   isLoading: state => state.INSERT_HOUSE_REDUCER.isLoading,
   isSuccess: state => state.INSERT_HOUSE_REDUCER.isSuccess,
   isFailure: state => state.INSERT_HOUSE_REDUCER.isFailure,
-  isData: state => state.INSERT_HOUSE_REDUCER.data,
+  isHouse: state => state.INSERT_HOUSE_REDUCER.data,
   isAgent: state => state.SELECT_AGENT_REDUCER.data,
   isImage: state => state.INSERT_IMAGE_REDUCER.data,
   isLogin: state => state.SELECT_LOGIN_REDUCER.data,
@@ -12,8 +12,10 @@ export const STATES = {
 };
 
 export const ACTIONS = {
+  ajaxDeleteAgent: params => actions.ajaxRequestDeleteAgent(params),
   ajaxInsertHouse: params => actions.ajaxRequestInsertHouse(params),
   deleteLayoutReplace: params => actions.deleteLayoutReplace(params),
+  updateAgentReplace: params => actions.updateAgentReplace(params),
   removeLayoutReplace: actions.removeLayoutReplace,
   removeImageReplace: actions.removeImageReplace
 };
